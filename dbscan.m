9f74a20cb3b43263b8bd224caae4b3cd4aa5e2df
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-01-30 18:06:39 +0100

    sizeOfCell vs n

--- dbscan.m
+++ dbscan.m
@@ -1,10 +1,10 @@
 function [result, noise] = dbscan(x, eps, minPts) 
 
     sizeOfCell = size(x,1);
-    result = zeros(n,1);
+    result = zeros(sizeOfCell,1);
     
     D = pdist2(x,x);
     
-    visited = false(n,1);
-    noise = false(n,1);
+    visited = false(sizeOfCell,1);
+    noise = false(sizeOfCell,1);
 end
