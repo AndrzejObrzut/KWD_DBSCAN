acaeb5bd9996859d20d12bd14b7dd9167746fff0
andrzej0221 <andrzejo94@gmail.com>
2016-01-31 01:44:48 +0100

    add data files .mat

--- main.m
+++ main.m
@@ -1,40 +1,45 @@
 clc;
 clear;
 
-iris = load('iris');
-SetOfPoints = iris.data;
+%data=load('example');
+%SetOfPoints = data.data;
 
-eps = 0.5;
-minPts = 10;
+data = load('localization');
+SetOfPoints = data.localization;
+
+%eps = 20;
+%minPts = 80;
+eps = 0.1;
+minPts = 25;
 
 result = dbscan(SetOfPoints, eps, minPts);
 
 max = max(result);
 Colors = hsv(max);
 
-Legends = {};
+Keys = {};
 for index = 0 : max
     Xi = SetOfPoints(result == index, :);
     if index ~= 0
         Style = '.';
         MarkerSize = 12;
         Color = Colors(index, :);
-        Legends{end+1} = ['Klaster nr' num2str(index)];
+        Keys{end+1} = ['Klaster nr' num2str(index)];
     else 
-        Style = 'x';
+        Style = '.';
         MarkerSize = 6;
         Color = [0 0 0];
-        if ~isempty(Xi)
-            Legends{end+1} = 'Szum';
+        if  isempty(Xi) == false
+            Keys{end+1} = 'Szum';
         end
     end
-    if ~isempty(Xi)
-        plot(Xi(:,1), Xi(:,2),Style,'MarkerSize',MarkerSize,'Color',Color);
+    if  isempty(Xi) == false
+        plot(Xi(:,2), Xi(:,1),Style,'MarkerSize',MarkerSize,'Color',Color);
     end
     hold on;
 end
 hold off;
 axis equal;
 grid on;
-legend(Legends);
+legend(Keys);
 legend('Location', 'NorthEastOutside');
